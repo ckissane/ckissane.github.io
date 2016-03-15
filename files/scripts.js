a9f4325dbdcdb76c4f5a27d82e94236d9248e744
coler706 <coler706@gmail.com>
2016-03-14 18:44:09 -0700

    df

--- files/scripts.js
+++ files/scripts.js
@@ -23,6 +23,24 @@ function fadeItIn(){
     
 
 
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400
+      
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+
 
 });
 
